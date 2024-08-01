964ee2a5425638165aaf3468deef97beefd2a873
rupsstar <50550957+rupsstar@users.noreply.github.com>
2024-08-01 16:31:51 +0530

    Update model.py
    updated the code

--- model.py
+++ model.py
@@ -8,7 +8,8 @@ iris = load_iris()
 X, y = iris.data, iris.target
 
 # Split dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.2, random_state=42)
 
 # Train model
 model = RandomForestClassifier()
